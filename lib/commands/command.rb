a99b03f5d6bc132950558c7851233a2f644312bf
Zachary Spector <zjspector@gmail.com>
2014-05-23 08:49:05 -0500

    Add add, edit, delete methods to command class

--- lib/commands/command.rb
+++ lib/commands/command.rb
@@ -6,6 +6,18 @@ module PL
       self.new.run(input)
     end
 
+    def self.add(input)
+      self.new.add(input)
+    end
+
+    def self.delete(input)
+      self.new.delete(input)
+    end
+
+    def self.edit(input)
+      self.new.edit(input)
+    end
+
     def failure(error_sym, data={})
       CommandFailure.new(data.merge :error => error_sym)
     end
