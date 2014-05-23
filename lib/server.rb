0c85bce4d296695bf2aca505c02f44070d25265c
Zachary Spector <zjspector@gmail.com>
2014-05-23 14:14:19 -0500

    Create homepage with create playlist option

--- lib/server.rb
+++ lib/server.rb
@@ -1,14 +1,29 @@
 require 'sinatra'
+require 'pry-debugger'
 require_relative 'pl.rb'
 
 set :bind, '0.0.0.0'
+
+enable :sessions
 
 get '/' do
   erb :login
 end
 
 post '/' do
-  erb :homepage
+  @username = params[:username]
+  @password = params[:password]
+  result = PL::LogInUser.run(name: @username, password: @password)
+  unless result.success?
+    @fail_message = result.error.to_s.gsub("_", " ")
+    erb :login
+  else
+    session[:logged_in] = true
+    session[:username] = @username
+    session[:password] = @password
+    @playlists = PL::ShowPlaylists.run(username: @username).playlists
+    erb :homepage, :layout => :layout_logged_in
+  end
 end
 
 get '/signup' do
@@ -18,14 +33,23 @@ end
 post '/signup' do
   @username = params[:username]
   @password = params[:password]
-  result = PL::CreateUser.run(username: @username, password: @password)
+  result = PL::CreateUser.run(name: @username, password: @password)
+  # binding.pry
  # check if user exists
   unless result.success?
     @fail_message = result.error.to_s.gsub("_", " ")
     erb :signup_fail
   else
+    session[:logged_in] = true
+    session[:username] = @username
+    session[:password] = @password
     erb :signup_success
     # posts success message here
     # creates button to complete login, redirects to homepage with session active
   end
+end
+
+get '/logout' do
+  session.clear
+  redirect to('/')
 end
