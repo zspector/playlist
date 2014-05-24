be9b64ce283cbe1379f6807a812b48a72173acf0
Christian Catalan <crcatala@gmail.com>
2014-05-24 17:19:31 -0500

    added user credentials check if blank, and auto redirect after registration

--- lib/commands/create_user.rb
+++ lib/commands/create_user.rb
@@ -1,7 +1,10 @@
+require 'pry-debugger'
 module PL
   class CreateUser < Command
     # inputs = { username: 'bob', password: 123}
     def run(inputs)
+      return failure(:input_blank) if inputs[:name] == "" || inputs[:password] == ""
+
       usernames = PL.db.get_user_by_name(inputs[:name])
       return failure(:name_taken) if usernames
       
